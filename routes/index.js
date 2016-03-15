b9dfa0d8210a9010e146e214d66a203022f8142e
wangsen <wangs@tongxinyiliao.com>
2016-03-15 11:17:45 +0800

    github oauth finish

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    github_client_id: '1c12b4a1306f5348dd81'
+  });
 });
 
 module.exports = router;
