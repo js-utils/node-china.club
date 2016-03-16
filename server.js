ebb8caaf09f13fee900b72f847c1f9053de1fb3f
wangsen <wangs@tongxinyiliao.com>
2016-03-16 18:03:08 +0800

    include ejs-mate

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
+var config = require('./config');
 var app = require('./app');
-var server = app.listen(3000, function(){
-	console.log('server start at:  http://localhost:3000');
+var server = app.listen(config.port, function(){
+	console.log('server start at:  http://localhost:' + config.port);
 });
