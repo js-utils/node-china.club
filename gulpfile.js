141600fefa8ee6b0f627253bb9415af1df290eff
wangsen <wangs@tongxinyiliao.com>
2016-03-21 15:25:24 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -19,13 +19,36 @@ mGulp.task('minjs', function(){
 });
 
 // 压缩css
+var mSass = require('gulp-ruby-sass');
 var minifycss = require('gulp-minify-css');
-mGulp.task('mincss', function(){
-    var cssSrc = './app/public/stylesheets/src/*.css',
-        cssDst = './app/public/stylesheets';
-    mGulp.src(cssSrc)
+var cssSrc = './app/public/stylesheets/src/*.scss';
+mGulp.task('sass', function(){
+    var cssDst = './app/public/stylesheets';
+
+    return mSass(cssSrc).on('error', function(err){
+       console.error('Error!', err.message);
+    })
         .pipe(mRename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(mGulp.dest(cssDst));
+});
 
-})
+//监听任务, 运行语句, gulp watch
+var mTinylr = require('tiny-lr');
+var server = mTinylr();
+var port = 35729;
+mGulp.task('watch', function(){
+    server.listen(port, function(err){
+        if(err) return err;
+
+        //监听css
+        mGulp.watch(cssSrc, function(){
+            mGulp.run('sass');
+        })
+    })
+});
+
+mGulp.task('default', function(){
+    // 先执行一般所有任务,然后执行监听
+    mGulp.run('sass', 'watch');
+});
