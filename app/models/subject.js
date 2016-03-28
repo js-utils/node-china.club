8e7e82b4bb8a80bcea4d4a227ee9b290a6c84307
五月的风 <578595193@qq.com>
2016-03-28 23:00:28 +0800

    add subject model

--- app/models/subject.js
+++ app/models/subject.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var SubjectSchema = new mongoose.Schema({
-    group: {
-        key: String,
-        value: String,
-        tags: Array
-    },
+    group: String,  //比如: 后台 ...
+    category: [{
+        key: String,   //比如: node meteor rails
+        value: String, //比如: Node.js Meteor Rails ...
+        tags: Array    // 比如: express gulp koa ...
+    }],
     updateAt: {type: Date, default: Date.now},
     createAt: {type: Date, default: Date.now}
 });
