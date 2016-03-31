1cef705e4936bd2eb49c4294e6864957c3a7561a
wangsen <wangs@tongxinyiliao.com>
2016-03-31 17:41:55 +0800

    post list

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var Post = require('../models/post');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express'
+
+  Post.findPosts(0, 0, 0, function(err, posts){
+    console.log(posts);
+    res.render('index', {
+      title: 'Express',
+      posts: posts || []
+    });
   });
 });
 
